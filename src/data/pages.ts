d139f252d46bb94ad9687725b6d6d1d9453a7a2b
Adam Rackis <adamrackis@hotmail.com>
2025-08-20 18:48:17 -0500

    pages index

---
+++ src/data/pages.ts
@@ -0,0 +1,29 @@
+import type { BTreeConfig } from "../types";
+
+export const indexConfig: BTreeConfig = {
+  keyColumns: ["pages"],
+  includeColumns: [],
+  data: [
+    { pages: 100 },
+    { pages: 150 },
+    { pages: 235 },
+    { pages: 342 },
+    { pages: 478 },
+    { pages: 521 },
+    { pages: 689 },
+    { pages: 127 },
+    { pages: 394 },
+    { pages: 612 },
+    { pages: 285 },
+    { pages: 456 },
+    { pages: 173 },
+    { pages: 598 },
+    { pages: 334 },
+    { pages: 267 },
+    { pages: 150 },
+    { pages: 235 },
+    { pages: 342 },
+    { pages: 478 },
+    { pages: 521 },
+  ],
+};
